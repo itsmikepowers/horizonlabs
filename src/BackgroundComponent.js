127bf6e2ba1f1bdb169985bb7805e5448df3a27b
Mike Powers <mikepowers@Mikes-MacBook-Air.local>
2023-09-05 18:48:37 -0400

    new

--- src/BackgroundComponent.js
+++ src/BackgroundComponent.js
@@ -1,76 +1,77 @@
-import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { Box } from '@chakra-ui/react';
+import { useSpring, animated } from 'react-spring';
 
-const ColoredCircle = ({ width, height, bgColor, top, left, animationName }) => {
-
-  // Add 10% more transparency 
-  const bgColorWithSmoothing = `radial-gradient(circle, ${bgColor} 0%, rgba(0,0,0,0) 71%)`;
-
-  return (
-    <Box
-      w={width}  
-      h={height}
-      bg={bgColorWithSmoothing} 
-
-      borderRadius="50%"
-      pos="absolute"
-      top={top}
-      left={left}
-      transform="translate(-50%, -50%)"
-      animation={`${animationName} 12s infinite`} 
-    />
-  );
-}
+const colors = ["#ff9050", "#25b6f2", "#fb4f59", "#4e2474"];
+const ColoredCircle = ({ width, height, initialColor, top, left }) => {
+    const [{ bgColor }, set] = useSpring(() => ({
+      bgColor: initialColor,
+      config: { tension: 280, friction: 280 }
+    }));
+  
+    // Start the color transition on mount
+    React.useEffect(() => {
+      let currentIndex = colors.indexOf(initialColor);
+      const timer = setInterval(() => {
+        currentIndex = (currentIndex + 1) % colors.length;
+        set({ bgColor: colors[currentIndex] });
+      }, 3000); // Change color every 4 seconds
+  
+      return () => clearInterval(timer);
+    }, [initialColor, set]);
+  
+    return (
+      <animated.div
+        style={{
+          width: width,
+          height: height,
+          background: bgColor.interpolate(color => `radial-gradient(circle, ${color} 0%, rgba(0,0,0,0) 71%)`),
+          borderRadius: '50%',
+          position: 'absolute',
+          top: top,
+          left: left,
+          transform: 'translate(-50%, -50%)'
+        }}
+      />
+    );
+  }
 
 const BackgroundComponent = ({ children }) => {
-
   return (
     <Box className="background-component" pos="relative" w="100%" h="100vh" >
-      
       <Box w="100%" h="100vh" pos="absolute" top={0} left={0} zIndex={0} overflow="hidden">
-      
         <ColoredCircle  
-          width="1000px"
-          height="1000px"
-          bgColor="#ff9050"
+          width="1200px"
+          height="1200px"
+          initialColor="#ff9050"
           top="60%"
           left="20%"
-          animationName="colorChangeOrange" 
         />
-
         <ColoredCircle
-          width="1200px" 
-          height="1200px"
-          bgColor="#25b6f2"
+          width="1400px" 
+          height="1400px"
+          initialColor="#25b6f2"
           top="30%"
           left="70%"
-          animationName="colorChangeBlue" 
         />
-        
         <ColoredCircle
-          width="1100px"
-          height="1100px"  
-          bgColor="#fb4f59"
+          width="1300px"
+          height="1300px"  
+          initialColor="#fb4f59"
           top="70%"
           left="90%"
-          animationName="colorChangeRed" 
         />
-
         <ColoredCircle
-          width="1000px"
-          height="1000px" 
-          bgColor="#4e2474"
+          width="1200px"
+          height="1200px" 
+          initialColor="#4e2474"
           top="15%"
           left="40%"
-          animationName="colorChangePurple" 
         />
-
       </Box>
-
       <Box zIndex={3}>
         {children}
       </Box>
-    
     </Box>
   );
 }
